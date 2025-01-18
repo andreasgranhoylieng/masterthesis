12d6552216fbcde3a6be92524797ac4e2ad569e7
andreasgranhoylieng <andreas.lieng@gmail.com>
2025-01-18 20:02:51 +0100

    make all .py to notebooks

--- download_datasets.py
+++
@@ -1,9 +0,0 @@
-from roboflow import Roboflow
-
-
-# Volume estimation dataset
-rf = Roboflow(api_key="WhO3q8dLKIclIg7IxQpe")
-project = rf.workspace("mastersmedical").project("syringe-volume-estimation")
-version = project.version(1)
-dataset = version.download("yolov8", location="datasets/Syringe-volume-estimation")
-                
--- volume_estimation/train_yolo.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# Load a model
-model = YOLO("../ultralytics_models/pose/yolo11n-pose.pt")
-results = model.train(
-    data="../datasets/Syringe-volume-estimation/data.yaml", epochs=10_000, imgsz=640, patience=100
-)
