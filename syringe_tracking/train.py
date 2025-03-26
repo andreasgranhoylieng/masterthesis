47b4b536809dccacd6af655bf60e7d0023c71b43
andreasgranhoylieng <andreas.lieng@gmail.com>
2025-03-26 18:21:01 +0100

    Train notebook to python script. delete notebook

---
+++ syringe_tracking/train.py
@@ -0,0 +1,20 @@
+from ultralytics import YOLO
+
+dataset_version = input("Enter the dataset version: ")
+model_type = input("Enter the model type: ")
+
+# Load a pre-trained YOLO11 model
+model = YOLO(f"yolo11{model_type}.pt") 
+
+# Train the model on your dataset
+results = model.train(
+    data="../datasets/Syringes/data.yaml", 
+    epochs=12000,
+    imgsz=1440,
+    patience=50,
+    workers=32,
+    device=[0, 1, 2],
+    batch=3*4,
+    augment=True,
+    name=f"train-OD11{model_type}-v{dataset_version}", 
+)
