e7f66de7ece7d0778b229c2d02e1a37c9d88c157
Andreas Granhøy-Lieng <andreas.lieng@gmail.com>
2025-03-29 14:55:26 +0100

    Add yolo12 compatibility to training pose

--- volume_estimation/train.py
+++ volume_estimation/train.py
@@ -1,10 +1,15 @@
 from ultralytics import YOLO
 
+yolo_version = input("Which version of yolo?: ")
 dataset_version = input("Enter the dataset version: ")
 model_type = input("Enter the model type: ")
 
-# Load a model
-model = YOLO(f"yolo11{model_type}-pose.pt")
+# Choose correct extension based on yolo_version
+ext = ".yaml" if yolo_version == "12" else ".stl"
+
+# Load the model with the chosen extension
+model = YOLO(f"yolo{yolo_version}{model_type}-pose{ext}")
+
 results = model.train(
     data="../datasets/Syringe-volume-estimation-yolo/data.yaml",
     epochs=1200,
@@ -12,7 +17,7 @@ results = model.train(
     patience=50,
     workers=32,
     device=[0, 1, 2],
-    batch=3*4,
+    batch=3 * 4,
     augment=True,
     flipud=0.0,
     fliplr=0.0,
@@ -20,5 +25,5 @@ results = model.train(
     single_cls=True,
     visualize=True,
     pose=50,
-    name=f"train-pose11{model_type}-v{dataset_version}",
+    name=f"train-pose{yolo_version}{model_type}-v{dataset_version}",
 )
